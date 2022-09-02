69f498d1a3c419658a51ea164a354f1ed7762c3c
Himanshu <himanshu064@gmail.com>
2022-09-02 16:54:35 +0530

    Added test email.

--- controller/misc.js
+++ controller/misc.js
@@ -1,19 +1,19 @@
 var nodemailer = require("nodemailer");
 exports.contactUs = async (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const phone = req.body.phone;
-    const message = req.body.message
+  const name = req.body.name;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const message = req.body.message;
   try {
     let htmlData = `<h5>UserName: ${name}</h5><h5>Email: ${email}</h5><h5>Phone: ${phone}</h5><h5>message:${message}</h5>`;
-   if(phone == undefined) {
-    htmlData = `<h5>UserName: ${name}</h5><h5>Email: ${email}</h5><h5>message:${message}</h5>`
-   }
+    if (phone == undefined) {
+      htmlData = `<h5>UserName: ${name}</h5><h5>Email: ${email}</h5><h5>message:${message}</h5>`;
+    }
     var transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         user: process.env.USER_EMAIL,
-        pass:  process.env.PASSWORD,
+        pass: process.env.PASSWORD,
       },
       port: 465,
       host: "smtp.gmail.com",
@@ -21,18 +21,19 @@ exports.contactUs = async (req, res) => {
 
     var mailOptions = {
       from: email,
-      to: process.env.USER_EMAIL,
+      to: process.env.TEST_EMAIL || process.env.USER_EMAIL,
       subject: "moivon contact us",
-      html: htmlData
+      html: htmlData,
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
-        res.status(400).send({status:"failed",error: error})
+        res.status(400).send({ status: "failed", error: error });
         console.log(error);
       } else {
-        res.status(200).send({status:"success",message: "email send successfully"})
-
+        res
+          .status(200)
+          .send({ status: "success", message: "email send successfully" });
       }
     });
   } catch (err) {
